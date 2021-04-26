57d3f15fd0b51e51fd43e3c9a911ef16f4b1682f
sowrishravigopal <sowrish.ravigopal@gmail.com>
2021-04-26 07:10:22 -0400

    changed width

--- sketch.js
+++ sketch.js
@@ -15,9 +15,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+     console.log;
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 305, 400, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
